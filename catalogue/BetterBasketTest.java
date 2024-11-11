b5b2687044ee3d843a249666372289fc4eeb2fb2
samuel <samueltsutcliffe@gmail.com>
2024-11-11 18:27:14 +0000

    added Junit test code

--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -1,10 +1,20 @@
 package catalogue;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 class BetterBasketTest {
 
     @org.junit.jupiter.api.Test
-    void add() {
-
+    void testMergeAddProduct() {
+        ArrayList<Product> br = new ArrayList<>();
+        Product p1 = new Product("0001", "toaster", 12.3, 1);
+        Product p2 = new Product("0002", "kettle", 10.00, 1);
+        br.add(p1);
+        br.add(p2);
+        br.add(p2);
+        br.add(p2);
+        assertEquals(2, br.size(),"incorrect size after merge");
+        assertEquals(3,br.get(1).getQuantity(),"incorrect quantity after merge");
     }
 }
