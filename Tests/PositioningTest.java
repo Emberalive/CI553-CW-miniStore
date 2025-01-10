65a460cb1ab3db0685b01aac9ee5c5cc524ec825
Sammy Boy <samueltsutcliffe@gmail.com>
2025-01-10 02:38:51 +0000

    removed redundant code for the Positioning test method for 1 screen

--- Tests/PositioningTest.java
+++ Tests/PositioningTest.java
@@ -3,7 +3,6 @@ package Tests;
 import Utils.Positioning;
 import org.junit.Test;
 import java.awt.*;
-import java.util.function.Predicate;
 import javax.swing.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -37,6 +36,8 @@ public class PositioningTest {
 
     }
 
+
+    //This test will only work if the computer has 1 screen, because I don't know how to simulate a graphics device
     @Test
     public void testMonLocation1Screen() {
         Positioning pos = new Positioning();
@@ -47,20 +48,6 @@ public class PositioningTest {
         ge.getScreenDevices();
         GraphicsDevice[] screens = ge.getScreenDevices();
 
-        int monitorIndex = 1;
-        if (monitorIndex < screens.length) {
-            GraphicsDevice selectedMonitor = screens[monitorIndex];
-            Rectangle bounds = selectedMonitor.getDefaultConfiguration().getBounds();
-
-            int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
-            int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
-
-            pos.SetMonitorStartUpMenu(window);
-
-            assertEquals(xPos, window.getLocation().x);
-            assertEquals(yPos, window.getLocation().y);
-        } else {
-            System.out.println("Defaulting to primary monitor");
             Rectangle bounds = screens[0].getDefaultConfiguration().getBounds();
             int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
             int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
@@ -69,6 +56,5 @@ public class PositioningTest {
             assertEquals(xPos, window.getLocation().x);
             assertEquals(yPos, window.getLocation().y);
         }
-
-    }
 }
+
