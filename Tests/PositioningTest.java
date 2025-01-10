7f89ddaa2b8f24e9408c31a37f1a5c5a2c297f2e
samuel <samueltsutcliffe@gmail.com>
2025-01-10 02:19:26 +0000

    Added my second test for the positioning class

--- Tests/PositioningTest.java
+++ Tests/PositioningTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PositioningTest {
 
     @Test
-    public void testMonLocation(){
+    public void testMonLocation() {
         Positioning pos = new Positioning();
         JFrame window = new JFrame();
         window.setSize(100, 100);
@@ -20,16 +20,55 @@ public class PositioningTest {
         GraphicsDevice[] screens = ge.getScreenDevices();
 
         int monitorIndex = 1;
-        GraphicsDevice selectedMonitor = screens[monitorIndex];
-        Rectangle bounds = selectedMonitor.getDefaultConfiguration().getBounds();
+        if (monitorIndex < screens.length) {
+            GraphicsDevice selectedMonitor = screens[monitorIndex];
+            Rectangle bounds = selectedMonitor.getDefaultConfiguration().getBounds();
 
-        int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
-        int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
+            int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
+            int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
 
-        pos.SetMonitorStartUpMenu(window);
+            pos.SetMonitorStartUpMenu(window);
 
-        assertEquals(xPos, window.getLocation().x);
-        assertEquals(yPos, window.getLocation().y);
+            assertEquals(xPos, window.getLocation().x);
+            assertEquals(yPos, window.getLocation().y);
+        } else {
+            System.out.println("No monitor selected");
+        }
+
     }
 
+    @Test
+    public void testMonLocation1Screen() {
+        Positioning pos = new Positioning();
+        JFrame window = new JFrame();
+        window.setSize(100, 100);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        ge.getScreenDevices();
+        GraphicsDevice[] screens = ge.getScreenDevices();
+
+        int monitorIndex = 1;
+        if (monitorIndex < screens.length) {
+            GraphicsDevice selectedMonitor = screens[monitorIndex];
+            Rectangle bounds = selectedMonitor.getDefaultConfiguration().getBounds();
+
+            int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
+            int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
+
+            pos.SetMonitorStartUpMenu(window);
+
+            assertEquals(xPos, window.getLocation().x);
+            assertEquals(yPos, window.getLocation().y);
+        } else {
+            System.out.println("Defaulting to primary monitor");
+            Rectangle bounds = screens[0].getDefaultConfiguration().getBounds();
+            int xPos = bounds.x + (bounds.width - window.getWidth()) / 2;
+            int yPos = bounds.y + (bounds.height - window.getHeight()) / 2;
+            pos.SetMonitorStartUpMenu(window);
+
+            assertEquals(xPos, window.getLocation().x);
+            assertEquals(yPos, window.getLocation().y);
+        }
+
+    }
 }
